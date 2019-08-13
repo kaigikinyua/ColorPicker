f3b0afa856eb419dd86178f8ef55ebf6c88fde22
kaigikinyua <kaigikinyua@gmail.com>
2019-08-13 23:43:55 +0300

    Application Initialization

---
+++ ColorPicker.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+class CPicker:
+	def __init__(self):
+		self.root=Tk()
+		self.root.title("ColorPicker")
+		self.root.mainloop()
+
+c=CPicker()
---
+++ DB.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+class DB:
+	def __init__(self):
+		try:
+			db=sqlite3.connect('./Colors.db')
+			print("Connected to database")
+		except:
+			print("Error Connecting to database")
+
+d=DB()
