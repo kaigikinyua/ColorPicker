f99898217b58ea243517a79820d25cc47061d276
kaigikinyua <kaigikinyua@gmail.com>
2020-10-10 21:45:20 +0300

    palet.py layout

---
+++ src/Palet.py
@@ -0,0 +1,15 @@
+import tk
+
+class Gui:
+    def __init__(self,palletName):
+        pass
+    
+    def render_pallet(self):
+        pass
+    
+    @staticmethod
+    def render_color(color,parrent):
+        pass
+
+    def copy_to_clipboard():
+        pass
