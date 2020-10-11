28fafaeb965b6fc15845c11ffdfc3d90db4d5344
kaigikinyua <kaigikinyua@gmail.com>
2020-10-11 20:53:14 +0300

    Notifications.py layout

---
+++ src/Notif.py
@@ -0,0 +1,15 @@
+import tk
+class Notif:
+    @staticmethod
+    def warning(message):
+        pass
+    @staticmethod
+    def error(message):
+        pass
+    @staticmethod
+    def success(message):
+        pass
+    @staticmethod
+    def message(message):
+        pass
+    
