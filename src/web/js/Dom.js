f344165a2f758f6027d0d6d04956908a76a56bcf
kaigikinyua <kaigikinyua@gmail.com>
2020-10-12 15:33:14 +0300

    open and close templates done

---
+++ src/web/js/Dom.js
@@ -0,0 +1,9 @@
+function openTemplates(){
+    var templates=document.getElementById("templates")
+    templates.style.display="block"
+    console.log("opening templates")
+}
+function closeTemplates(){
+    var templates=document.getElementById("templates")
+    templates.style.display="none"
+}
